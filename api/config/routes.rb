97aa06286f67825d19718d3b657444f071a35cda
erico dias <ericodias1@gmail.com>
2015-05-08 20:07:45 -0300

    #users model created

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :v1  do
     # users routes
     get 'users' => 'users#show'
+    get 'users/create' => 'users#create'
   end
 end
--- api/test/controllers/v1/users_controller_test.rb
+++ api/test/controllers/v1/users_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class V1::UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    include Devise::TestHelpers
+  test "new_user" do
+    put 'create', {:controller => 'v1/users'}
+    assert_response :success
+    resp = JSON.parse response.body
+    assert_not_nil resp
+  end
 end
