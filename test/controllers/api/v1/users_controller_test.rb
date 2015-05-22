803fc3499a56b21191f27fe94f1d0bd63da318b1
erico dias <ericodias1@gmail.com>
2015-05-21 21:57:51 -0300

    removendo restrições

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "show" do
+    get :show, {controller: 'api/v1/users', id: 1, format: :json}
+    assert_response :success
+    response = JSON.parse @response.body
+  end
+
+
 end
