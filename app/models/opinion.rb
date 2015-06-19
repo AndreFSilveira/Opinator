91cb1e5bc55a40729fd024c6d48f9e50a946b994
André Felipe Silveira <andrefelipesilveira@yahoo.com.br>
2015-06-19 19:08:58 -0300

    alter opinion filter title

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,10 +1,10 @@
 class Opinion < ActiveRecord::Base
 	validates_presence_of :description, :user_id, :approved, :title
-  validates :description, length: { minimum: 10 }
+    validates :description, length: { minimum: 10 }
 	validates :title, length: { minimum: 5 }
 
 	belongs_to :user
-  has_many :disagree
+    has_many :disagree
 	has_many :agree
 	# has_many :comment
 end
