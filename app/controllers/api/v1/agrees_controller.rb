57da3c57ba79ae44289d1883f8089d3e3c4cf922
erico dias <ericodias1@gmail.com>
2015-06-20 18:25:46 -0300

    bugfix

--- app/controllers/api/v1/agrees_controller.rb
+++ app/controllers/api/v1/agrees_controller.rb
@@ -1,10 +1,6 @@
 class Api::V1::AgreesController < ApplicationController
     before_action :set_agree, only: [:show, :update, :destroy]
 
-    def set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = '*'
-    end
-
     def index
         @agrees = Agree.all
         respond_to do |format|
@@ -13,37 +9,38 @@ class Api::V1::AgreesController < ApplicationController
     end
 
     def create
-        @Agree = Agree.new agree_params
+        p request.raw_post.inspect
+        @agree = Agree.new agree_params
         respond_to do |format|
-            if @Agree.save
-                format.json { render :json => @Agree.to_json }
+            if @agree.save
+                format.json { render :json => @agree.to_json }
             else
-                format.json { render :json => @Agree.errors.to_json }
+                format.json { render :json => @agree.errors.to_json }
             end
         end
     end
 
     def show
         respond_to do |format|
-          format.json { render :json => @Agree ? @Agree : record_not_found }
+          format.json { render :json => @agree ? @agree : record_not_found }
         end
     end
 
     def update
         respond_to do |format|
-            if @Agree.update agree_params
-                format.json { render :json => @Agree.to_json }
+            if @agree.update agree_params
+                format.json { render :json => @agree.to_json }
             else
-                format.json { render :json => @Agree.errors.to_json }
+                format.json { render :json => @agree.errors.to_json }
             end
         end
     end
 
     def destroy
         respond_to do |format|
-            if @Agree
-                @Agree.destroy
-                format.json { render :json => @Agree.to_json }
+            if @agree
+                @agree.destroy
+                format.json { render :json => @agree.to_json }
             else
                 format.json { render :json => record_not_found }
             end
@@ -52,10 +49,16 @@ class Api::V1::AgreesController < ApplicationController
 
     private
     def agree_params
-        params.require(:agree).permit(:user_id, :opinion_id)
+        if(!params[:agree])
+            agree_hash = JSON.parse(request.raw_post)
+            agree = agree_hash['agree']
+            {user_id: agree['user_id'], opinion_id: agree['opinion_id']}
+        else
+            params.require(:agree).permit(:user_id, :opinion_id)
+        end
     end
 
     def set_agree
-        @Agree = Agree.find_by_id(params[:id])
+        @agree = Agree.find_by_id(params[:id])
     end
 end
